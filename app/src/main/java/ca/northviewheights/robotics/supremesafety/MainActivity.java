638b8e3288f01e1591cbfe274b8cefa353be3d17
jddhulia <jddhulia@uwaterloo.ca>
2019-01-26 18:43:42 -0500

    update front page

--- app/src/main/java/ca/northviewheights/robotics/supremesafety/MainActivity.java
+++ app/src/main/java/ca/northviewheights/robotics/supremesafety/MainActivity.java
@@ -21,19 +21,19 @@ public class MainActivity extends AppCompatActivity {
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Futura.ttf");
         TextView textView = (TextView) findViewById(R.id.textView2);
         textView.setTypeface(typeface);
-        TextView textView2 = (TextView) findViewById(R.id.textView4);
-        textView2.setTypeface(typeface);
-        TextView textView3 = (TextView) findViewById(R.id.textView5);
-        textView3.setTypeface(typeface);
-        Button buttonA = (Button) findViewById(R.id.button);
-        buttonA.setTypeface(typeface);
-        TextView version = (TextView) findViewById(R.id.textView9);
-        version.setText("Version: " + BuildConfig.VERSION_NAME);
-        TextView website =(TextView)findViewById(R.id.textView13);
-        website.setClickable(true);
-        website.setMovementMethod(LinkMovementMethod.getInstance());
-        String link = "<a href='https://rebrand.ly/supreme'> Website </a>";
-        website.setText(Html.fromHtml(link));
+//        TextView textView2 = (TextView) findViewById(R.id.textView4);
+//        textView2.setTypeface(typeface);
+//        TextView textView3 = (TextView) findViewById(R.id.textView5);
+//        textView3.setTypeface(typeface);
+//        Button buttonA = (Button) findViewById(R.id.button);
+//        buttonA.setTypeface(typeface);
+//        TextView version = (TextView) findViewById(R.id.textView9);
+//        version.setText("Version: " + BuildConfig.VERSION_NAME);
+//        TextView website =(TextView)findViewById(R.id.textView13);
+//        website.setClickable(true);
+//        website.setMovementMethod(LinkMovementMethod.getInstance());
+//        String link = "<a href='https://rebrand.ly/supreme'> Website </a>";
+//        website.setText(Html.fromHtml(link));
     }
 
     /** Called when the user taps the Send button */
