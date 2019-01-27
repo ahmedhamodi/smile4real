2d8b0618cbb81501c233874e5b4da72291b3b771
unknown <amoliksingh@hotmail.com>
2019-01-26 23:06:27 -0500

    Fixed screen dependency bug

--- app/src/main/java/ca/northviewheights/robotics/supremesafety/MainActivity.java
+++ app/src/main/java/ca/northviewheights/robotics/supremesafety/MainActivity.java
@@ -1,9 +1,13 @@
 package ca.northviewheights.robotics.supremesafety;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,6 +15,11 @@ import android.widget.TextView;
 import android.text.method.LinkMovementMethod;
 import android.text.Html;
 import ca.northviewheights.robotics.supremesafety.BuildConfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -48,8 +57,67 @@ public class MainActivity extends AppCompatActivity {
         //intent.putExtra(EXTRA_MESSAGE, message);
         //startActivity(intent);
     }
+    public void goToActivity1 (View view){
+        CopyReadAssets("manual.pdf");
+    }
+
     public void goToActivity2 (View view){
-        Intent intent = new Intent (this, DisplayMessageActivity.class);
+        CopyReadAssets("brochure.pdf");
+    }
+
+    private void CopyReadAssets(String name)
+    {
+        AssetManager assetManager = getAssets();
+
+        InputStream in = null;
+        OutputStream out = null;
+        File file = new File(getFilesDir(), name);
+        try
+        {
+            in = assetManager.open(name);
+            out = openFileOutput(file.getName(), Context.MODE_WORLD_READABLE);
+
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+        } catch (Exception e)
+        {
+            Log.e("tag", e.getMessage());
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(
+                Uri.parse("file://" + getFilesDir() + "/" + name),
+                "application/pdf");
+
+        startActivity(intent);
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1)
+        {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    public void goToActivity3 (View view){
+        Intent intent = new Intent (this, DisplayMessageActivity2.class);
+        startActivity(intent);
+    }
+
+    public void goToActivity4 (View view){
+        Intent intent = new Intent (this, Quiz.class);
+        startActivity(intent);
+    }
+
+    public void goToActivity5 (View view){
+        Intent intent = new Intent (this, Gallery.class);
         startActivity(intent);
     }
 
