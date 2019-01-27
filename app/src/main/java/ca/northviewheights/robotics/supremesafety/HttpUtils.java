34c0cfca1c90c14e75612f40be1303afca110e8b
jddhulia <jddhulia@uwaterloo.ca>
2019-01-26 21:38:34 -0500

    update get request

--- app/src/main/java/ca/northviewheights/robotics/supremesafety/HttpUtils.java
+++ app/src/main/java/ca/northviewheights/robotics/supremesafety/HttpUtils.java
@@ -4,6 +4,7 @@ import android.widget.TextView;
 
 import com.loopj.android.http.*;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class HttpUtils {
@@ -38,15 +39,50 @@ public class HttpUtils {
         RequestParams rp = new RequestParams();
         rp.add("category","inspire");
         JsonHttpHandler jp = new JsonHttpHandler();
-        HttpUtils.get("t", rp,jp);
-
+        //HttpUtils.get("t", rp,jp);
+        callback(new JSONObject());
 //        byte[] buffer;
 //        jp.getResponseString(buffer, )
         return jp.response_string.toString();
     }
 
     public static void callback(JSONObject obj){
-        tv.setText(obj.toString()); 
-        System.out.println("Here");
+
+        String json_test = "{\n" +
+                "  \"success\": {\n" +
+                "    \"total\": 1\n" +
+                "  },\n" +
+                "  \"contents\": {\n" +
+                "    \"quotes\": [\n" +
+                "      {\n" +
+                "        \"quote\": \"When I was a little kid, I was really scared of the dark. But then I came to understand, dark just means the absence of photons in the visible wavelength—400 to 700 nanometers. Then I thought, well it’s really silly to be afraid of a lack of photons. Then I wasn’t afraid of the dark anymore after that.\",\n" +
+                "        \"author\": \"Elon Musk\",\n" +
+                "        \"length\": \"303\",\n" +
+                "        \"tags\": [\n" +
+                "          \"fear\",\n" +
+                "          \"inspire\"\n" +
+                "        ],\n" +
+                "        \"category\": \"inspire\",\n" +
+                "        \"title\": \"Inspiring Quote of the day\",\n" +
+                "        \"date\": \"2019-01-26\",\n" +
+                "        \"id\": null\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"copyright\": \"2017-19 theysaidso.com\"\n" +
+                "  }\n" +
+                "}";
+
+        try {
+            obj = new JSONObject(json_test);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            tv.setText(obj.getJSONObject("contents").getJSONArray("quotes").getJSONObject(0).get("quote").toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //System.out.println("Here");
     }
 }
